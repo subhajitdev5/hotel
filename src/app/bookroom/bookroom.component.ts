92ca205b142ee2d860c9983871d92d8484decf31
lavanya <kadiyala.baby-lavanya@siemens.com>
2024-10-08 21:28:48 +0530

    initial commit

---
+++ src/app/bookroom/bookroom.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookroom',
+  templateUrl: './bookroom.component.html',
+  styleUrl: './bookroom.component.css'
+})
+export class BookroomComponent {
+  bookroom(){
+
+  }
+  cancelroom(){
+    
+  }
+
+}
